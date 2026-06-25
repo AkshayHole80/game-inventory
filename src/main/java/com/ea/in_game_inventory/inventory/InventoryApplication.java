de1197ca43442cdc4f69d95abc819a4ce038855c
Akshay Hole <akshay.hole@p99soft.com>
2026-06-25 22:47:39 +0530

    Initial project setup commit

---
+++ src/main/java/com/ea/in_game_inventory/inventory/InventoryApplication.java
@@ -0,0 +1,13 @@
+package com.ea.in_game_inventory.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+
+}
